527f1bb3ce3610c6199783b6de5f5550642e9cd7
Eugene <panteleymonchuk@gmail.com>
2020-04-08 21:30:16 +0300

    Removed lock file.

--- gulpfile.js
+++ gulpfile.js
@@ -11,33 +11,33 @@ var less = require('gulp-less');
 
 
 gulp.task('sass', function () {
-    return gulp.src('app/scss/**/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe( sass().on('error', sass.logError) )
-        .pipe(autoprefixer())
-        .pipe( gulp.dest('app/css/') )
-        .pipe( rename('style.min.css') )
-        .pipe( cleanCSS({compatibility: 'ie8'}) )
-        .pipe(sourcemaps.write('.'))
-        .pipe( gulp.dest('app/css/') )
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+  return gulp.src('app/scss/**/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('app/css/'))
+    .pipe(rename('style.min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('app/css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
-    })
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/**/*.html', browserSync.reload);
-    gulp.watch('app/**/*.css', browserSync.reload);
-    gulp.watch('app/**/*.js', browserSync.reload);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/**/*.html', browserSync.reload);
+  gulp.watch('app/**/*.css', browserSync.reload);
+  gulp.watch('app/**/*.js', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'browserSync']);
